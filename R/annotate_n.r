fc91a93d4fe61b3700011ccd26375fd44ca545f9
William Koehler <williamk03hler@gmail.com>
2017-10-18 11:14:46 -0700

    relooking at annotate_n
    Redoing annotate_n as an r script

---
+++ R/annotate_n.r
@@ -0,0 +1,47 @@
+geneXtender <- function(organism, upstream) {
+  messy2 <- dplyr::filter(organism, type == "gene")
+  neat <- dplyr::select(messy2, seqid, start, end, strand, gene_id, gene_name)
+  pos_exons <- dplyr::filter(neat, strand == "+")
+  neg_exons <- dplyr::filter(neat, strand == "-")
+  pos_exons$start = pos_exons$start - upstream
+  pos_exons$start[pos_exons$start < 0] <- 1
+  pos_exons$end = pos_exons$end + 500
+  neg_exons$start = neg_exons$start - 500
+  neg_exons$start[neg_exons$start < 0] <- 1
+  neg_exons$end = neg_exons$end + upstream
+  merged_exons <- rbind(pos_exons, neg_exons)
+  geneXtender.file <- dplyr::select(merged_exons, seqid, start, end, gene_id, gene_name)
+  levels(geneXtender.file$seqid) <- gsub("chr", "", levels(geneXtender.file$seqid))
+  levels(geneXtender.file$seqid) <- gsub("X", "100", levels(geneXtender.file$seqid))
+  levels(geneXtender.file$seqid) <- gsub("Y", "200", levels(geneXtender.file$seqid))
+  levels(geneXtender.file$seqid) <- gsub("MT", "300", levels(geneXtender.file$seqid))
+  levels(geneXtender.file$seqid) <- gsub("MtDNA", "300", levels(geneXtender.file$seqid))
+  levels(geneXtender.file$seqid) <- gsub("M", "300", levels(geneXtender.file$seqid))
+  levels(geneXtender.file$seqid) <- gsub("Mito", "300", levels(geneXtender.file$seqid))
+  levels(geneXtender.file$seqid) <- gsub("I", "1", levels(geneXtender.file$seqid))
+  levels(geneXtender.file$seqid) <- gsub("II", "2", levels(geneXtender.file$seqid))
+  levels(geneXtender.file$seqid) <- gsub("III", "3", levels(geneXtender.file$seqid))
+  levels(geneXtender.file$seqid) <- gsub("IV", "4", levels(geneXtender.file$seqid))
+  levels(geneXtender.file$seqid) <- gsub("V", "5", levels(geneXtender.file$seqid))
+  levels(geneXtender.file$seqid) <- gsub("VI", "6", levels(geneXtender.file$seqid))
+  levels(geneXtender.file$seqid) <- gsub("VII", "7", levels(geneXtender.file$seqid))
+  levels(geneXtender.file$seqid) <- gsub("VIII", "8", levels(geneXtender.file$seqid))
+  levels(geneXtender.file$seqid) <- gsub("IX", "9", levels(geneXtender.file$seqid))
+  levels(geneXtender.file$seqid) <- gsub("XI", "11", levels(geneXtender.file$seqid))
+  levels(geneXtender.file$seqid) <- gsub("XII", "12", levels(geneXtender.file$seqid))
+  levels(geneXtender.file$seqid) <- gsub("XIII", "13", levels(geneXtender.file$seqid))
+  levels(geneXtender.file$seqid) <- gsub("XIV", "14", levels(geneXtender.file$seqid))
+  levels(geneXtender.file$seqid) <- gsub("XV", "15", levels(geneXtender.file$seqid))
+  levels(geneXtender.file$seqid) <- gsub("XVI", "16", levels(geneXtender.file$seqid))
+  geneXtender.file$seqid = as.numeric(as.character(geneXtender.file$seqid))
+  geneXtender.sorted <- dplyr::arrange(geneXtender.file, as.numeric(seqid), start)
+  write.table(geneXtender.sorted, quote = FALSE, sep = "\t", row.names = FALSE, col.names = FALSE, sprintf("geneXtender_gtf_%s.bed", upstream))
+}
+
+annotate_n <- function(organism, extension, n=2, cap=Inf) {
+  if(!file.exists("peaks.txt")){
+    message("Please run peaksInput() function first!  See ?peaksInput for more information")
+  } else {
+    peaks <- fread("peaks.txt")
+  }
+}
